c56e6045ccbb85575865eeda7047fe35247f2c80
AbinayaArumugam <47531319+AbinayaArumugam@users.noreply.github.com>
2021-09-20 10:47:07 +0530

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -43,6 +43,41 @@ public class Runner {
             case 6:
                 System.out.println(MinimumSwapToSort.minSwap(arr));
                 break;
+                  case 7:
+                SumClosestToZero.findClosestNumber(arr);
+                break;
+            case 8:
+                System.out.println(SortedOrNot.checkSort(arr));
+                break;
+            case 9:
+                System.out.println("Enter the Array length");
+                int length9=in.nextInt();
+                int[] arr9=new int[length9];
+                for (int i=0;i<length9;i++){
+                    arr9[i]=in.nextInt();
+                }
+                System.out.println(ArraysEqualOrNot.checkArraysAreEqual(arr,arr9));
+                break;
+            case 10:
+                System.out.println("Enter the key");
+                int key=in.nextInt();
+                Arrays.sort(arr);
+                System.out.println(MaximumToys.MaxToys(arr,length,key));
+                break;
+            case 11:
+                System.out.println("Enter the String");
+                String str1=in.next();
+                System.out.println("Enter the String");
+                String str2=in.next();
+                System.out.println(Anagram.checkAnagram(str1,str2));
+                break;
+            case 12:
+                System.out.println("Enter the key");
+                int key12=in.nextInt();
+                Arrays.sort(arr);
+                System.out.println(ShopInCandyStore.findMin(arr,length,key12)+" "+ShopInCandyStore.findMax(arr,length,key12));
+                break;
+                
         }
     }
 }
